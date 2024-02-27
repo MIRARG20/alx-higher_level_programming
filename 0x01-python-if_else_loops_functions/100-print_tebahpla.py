99e43fc9bf4a941157dca0037d08b90a89be198a
MIRARG20 <amiraragab480@gmail.com>
2024-02-27 01:39:55 -0800

    task14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for letter in range(90, 64, -1):
-    if letter % 2 == 0:
-        i = 32
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        diff = 0
     else:
-        i = 0
-    print(f"{chr(letter+i)}", end="")
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
